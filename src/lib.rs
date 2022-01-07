20492b94f7584a031a3efcddff7814e85c225478
jprochazk <honza.spacir@gmail.com>
2022-01-07 18:53:49 +0100

    re-add contents of `src/lib.rs` and `Cargo.toml`

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,30 @@
+pub mod client;
+pub mod error;
+pub mod peer;
+pub mod server;
 
+// TODO: prelude
+
+mod packet;
+mod queue;
+mod seq;
+mod socket;
+
+use std::{
+  collections::hash_map::DefaultHasher,
+  hash::{Hash, Hasher},
+};
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Copy, PartialEq, Serialize, Deserialize)]
+#[serde(transparent)]
+pub struct Protocol(u64);
+
+impl<T: Hash> From<T> for Protocol {
+  fn from(v: T) -> Self {
+    let mut s = DefaultHasher::new();
+    v.hash(&mut s);
+    Self(s.finish())
+  }
+}
